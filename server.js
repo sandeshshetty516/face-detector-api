d346ba735c1fe8dbb4d0203a0a31c3618d11ff9f
Sandesh Shetty <sandesh.s@pacewisdom.com>
2021-10-04 17:00:43 +0530

    database name

--- server.js
+++ server.js
@@ -11,16 +11,10 @@ const image = require('./controllers/image');
 const db =knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : 'postgres',
-      password : 'sandy',
-      database : 'facedetection'
+      connectionString : process.env.DATABASE_URL,
+      ssl: true,
     }
 });
-
-db.select('*').from('users').then(data => {
-    console.log(data);
-})
 
 
 const app = express();
